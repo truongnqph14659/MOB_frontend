344c7e4145fe0fd6fb212cd3678787f77e636ef0
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-10-03 09:22:30 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { HomeadminComponent } from './components/admin/homeadmin/homeadmin.component';
+import { AnalyticsComponent } from './components/admin/analytics/analytics.component';
+import { HeaderpageComponent } from './components/admin/headerpage/headerpage.component';
+import { FooterpageComponent } from './components/admin/footerpage/footerpage.component';
+import { SiderbarComponent } from './components/admin/siderbar/siderbar.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    HomeadminComponent,
+    AnalyticsComponent,
+    HeaderpageComponent,
+    FooterpageComponent,
+    SiderbarComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/admin/headerpage/headerpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headerpage',
+  templateUrl: './headerpage.component.html',
+  styleUrls: ['./headerpage.component.css']
+})
+export class HeaderpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
