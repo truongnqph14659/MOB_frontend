b810a772e5c7e322d8bd79be19c559b383981c0e
= <huynguyenz2002@gmail.com>
2022-10-11 08:32:06 +0700

    Layout

---
+++ src/app/components/admin/statistical/statistical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistical',
+  templateUrl: './statistical.component.html',
+  styleUrls: ['./statistical.component.css']
+})
+export class StatisticalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
